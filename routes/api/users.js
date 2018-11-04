c6813ab00eb23639b744e16dfced71a14692c16f
Francisco Arredondo <frarredo@gmail.com>
2018-11-03 20:29:48 -0700

    changed Users model to sigular form User

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,12 +11,12 @@ const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 
 // Load User model
-const User = require("../../models/Users");
+const User = require("../../models/User");
 
 // @route   GET api/users/test
-// @desc    Tests users route
+// @desc    Tests user route
 // @access  Public
-router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+router.get("/test", (req, res) => res.json({ msg: "User works" }));
 
 // @route   GET api/users/register
 // @desc    Register user
