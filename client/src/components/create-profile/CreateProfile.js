b8717a33356478d7c45a8ccb914e0b5abd0ad519
Francisco Arredondo <frarredo@gmail.com>
2018-11-11 20:44:47 -0800

    removed duplicate import statement from CreateProfile component

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -5,7 +5,6 @@ import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import InputGroup from "../common/InputGroup";
 import SelectListGroup from "../common/SelectListGroup";
-import TextFieldGroup from "../common/TextFieldGroup";
 
 class CreateProfile extends Component {
   constructor(props) {
