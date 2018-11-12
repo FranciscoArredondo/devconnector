aed9dc8f83c25dbc92220739042cc44aafa7ab17
Francisco Arredondo <frarredo@gmail.com>
2018-11-12 15:16:19 -0800

    added EditProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import CreateProfile from "./components/create-profile/CreateProfile";
 
 import "./App.css";
 import { clearCurrentProfile } from "./actions/profileActions";
+import EditProfile from "./components/edit-profile/EditProfile";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -58,6 +59,13 @@ class App extends Component {
                   exact
                   path="/create-profile"
                   component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
                 />
               </Switch>
             </div>
