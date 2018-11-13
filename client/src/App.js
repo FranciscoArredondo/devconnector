182321dc5d97d07b1da516595bce0fc6c3b5d895
Francisco Arredondo <frarredo@gmail.com>
2018-11-13 14:55:42 -0800

    added education form and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -19,23 +19,24 @@ import "./App.css";
 import { clearCurrentProfile } from "./actions/profileActions";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 // Check for token
 if (localStorage.jwtToken) {
-  // Set auth token header
-  setAuthToken(localStorage.jwt_decode);
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
   // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
 
   // Check for expired token
-  const currentTime = Date.now / 1000;
+  const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
-    // Logout the user
+    // Logout user
     store.dispatch(logoutUser());
+    // Clear current Profile
     store.dispatch(clearCurrentProfile());
-
     // Redirect to login
     window.location.href = "/login";
   }
@@ -74,6 +75,13 @@ class App extends Component {
                   exact
                   path="/add-experience"
                   component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
                 />
               </Switch>
             </div>
