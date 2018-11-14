96e9c4f3f8deeb6d31d382ab2d4417509aa8d6a2
Francisco Arredondo <frarredo@gmail.com>
2018-11-14 14:45:26 -0800

    added clearErrors redux action

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,4 @@
-export const TEST_DISPATCH = "TEST_DISPATCH";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const GET_ERRORS = "GET_ERRORS";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const GET_PROFILE = "GET_PROFILE";
