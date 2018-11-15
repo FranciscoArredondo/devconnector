8731e348957f418d36337eb5be1511eeafa4fab4
Francisco Arredondo <frarredo@gmail.com>
2018-11-15 15:19:49 -0800

    fixed a few typos to ProfileGithub component

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -21,17 +21,15 @@ class ProfileGithub extends Component {
     const { count, sort, clientId, clientSecret } = this.state;
 
     fetch(
-      `http://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&clientSecret=${clientSecret}`
-    ).then(res =>
-      res
-        .json()
-        .then(data => {
-          this.setState({
-            repos: data
-          });
-        })
-        .catch(err => console.log(err))
-    );
+      `https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        if (this.refs.myRef) {
+          this.setState({ repos: data });
+        }
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -53,20 +51,19 @@ class ProfileGithub extends Component {
               Stars: {repo.stargazers_count}
             </span>
             <span className="badge badge-secondary mr-1">
-              Stars: {repo.watchers_count}
+              Watchers: {repo.watchers_count}
             </span>
-            <span className="badge badge-success mr-1">
-              Stars: {repo.forks_count}
+            <span className="badge badge-success">
+              Forks: {repo.forks_count}
             </span>
           </div>
         </div>
       </div>
     ));
-
     return (
-      <div>
+      <div ref="myRef">
         <hr />
-        <h3 className="mb-3">Latest Github Repos</h3>
+        <h3 className="mb-4">Latest Github Repos</h3>
         {repoItems}
       </div>
     );
